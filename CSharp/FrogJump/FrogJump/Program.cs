33597b50f24b72aa77102751bb77789e047d9b34
go4zee <go4zeee@gmail.com>
2016-09-07 13:23:45 -0400

    Add files via upload

---
+++ CSharp/FrogJump/FrogJump/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogJump
+{
+    class Program
+    {
+        public int solution(int X, int Y, int D)
+        {
+            int retVal = 0;
+
+            if ((Y - X) % D == 0)
+                retVal = (Y - X) / D;
+            else
+                retVal = ((Y - X) / D) + 1;
+
+
+            return retVal;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
